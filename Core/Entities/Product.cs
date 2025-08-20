eee23c6f7abf8876a171c8276e70007eb5c9b0dd
Furkan Şahin Varışlı <varislifurkansahin@gmail.com>
2025-08-20 21:40:34 +0300

    entities created

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public List<string> ImageUrl { get; set; }
+        [Column(TypeName = "varchar(150)")]
+        public string Description { get; set; }
+    }
+}
---
+++ Core/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Entities
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
